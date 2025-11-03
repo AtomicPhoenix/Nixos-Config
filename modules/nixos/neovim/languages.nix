8db98478ca3ed0980a8206457fac10439233e61c
Possiblyai <imrana50280@gmail.com>
2025-11-03 03:08:37 -0500

    Cleaned code

--- modules/nixos/neovim/languages.nix
+++ modules/nixos/neovim/languages.nix
@@ -15,13 +15,8 @@ in {
   go = enableLanguage;
   bash = enableLanguage;
   clang = enableLanguage;
-  ts = enableLanguage // {format.type = "prettierd";};
-  css = enableLanguage // {format.type = "prettierd";};
   html.enable = true;
-
-  markdown = {
-    enable = true;
-    lsp.enable = true;
-    extensions.render-markdown-nvim.enable = true;
-  };
+  css = enableLanguage // {format.type = "prettierd";};
+  ts = enableLanguage // {format.type = "prettierd";};
+  markdown = enableLanguage // {extensions.render-markdown-nvim.enable = true;};
 }
