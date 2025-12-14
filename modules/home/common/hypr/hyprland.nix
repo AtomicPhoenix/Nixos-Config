56face9de47539f2b88b05ff9c233b19c012851d
Possiblyai <imrana50280@gmail.com>
2025-12-13 20:43:43 -0500

    Cleaned Hypridle Configuration (Fixed/added comments, removed unnecessary parts

--- modules/home/common/hypr/hyprland.nix
+++ modules/home/common/hypr/hyprland.nix
@@ -340,11 +340,15 @@
     settings = {
       # General settings for what to do when certain global events / signals occur (e.g. lock_cmd runs when a lock signal is heard)
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock;"; # avoid starting multiple hyprlock instances.
-        unlock_cmd = "pkill hyprlock;";
-        before_sleep_cmd = "hyprlock;";
-        after_sleep_cmd = "hyprctl dispatch dpms on; nmcli radio wifi on; brightnessctl -d intel_backlight s 100% &&  brightnessctl -d card1-eDP-2-backlight s $(brightnessctl -d intel_backlight g)"; # to avoid having to press a key twice to turn on the display.
-        ignore_dbus_inhibit = false; # Don't dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
+        lock_cmd = "pidof hyprlock || hyprlock &;"; # avoid starting multiple hyprlock instances.
+        before_sleep_cmd = "loginctl lock-session;";
+        after_sleep_cmd = ''
+          hyprctl dispatch dpms on;
+          nmcli radio wifi on;
+          brightnessctl -d intel_backlight s 100%;
+          brightnessctl -d card1-eDP-2-backlight s $(brightnessctl -d intel_backlight g);
+        '';
+        ignore_dbus_inhibit = false; # Don't ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
         ignore_systemd_inhibit = false; # Don't ignore systemd-inhibit --what=idle inhibitors
         ignore_wayland_inhibit = false; # Don't ignore wayland protocol inhibition requests (e.g. from running video players)
       };
@@ -361,7 +365,7 @@
         }
         {
           timeout = 300;
-          on-timeout = "systemd-ac-power || hyprctl dispatch dpms off";
+          on-timeout = "systemd-ac-power || hyprctl dispatch dpms off"; # Turns DPMS off when on battery
           on-resume = "hyprctl dispatch dpms on";
         }
         {
