591b1891f427de234cde757fad16ddf9ae2aac03
Possiblyai <imrana50280@gmail.com>
2026-02-19 17:06:30 -0500

    Split hyprland windowrules into its own file

--- modules/home/common/hypr/hyprland.nix
+++ modules/home/common/hypr/hyprland.nix
@@ -286,59 +286,7 @@ _: {
         "$mainMod, mouse:273, resizewindow"
       ];
 
-      # Connect Mouse
-      # bind = $mainMod SHIFT, B, exec, /usr/bin/BT5MouseConnect.sh
-
-      ##############################
-      ### WINDOWS AND WORKSPACES ###
-      ##############################
-
-      windowrule = [
-        {
-          name = "Firefox PiP";
-          "match:initial_class" = "firefox";
-          "match:initial_title" = "^Picture.*$";
-          pin = "on";
-          float = "on";
-          size = "(monitor_w*.4) (monitor_h*.4)";
-        }
-        {
-          name = "mpv";
-          "match:class" = "mpv";
-          pin = "on";
-          float = "on";
-          size = "(monitor_w*.4) (monitor_h*.4)";
-        }
-        {
-          name = "Discord";
-          "match:initial_class" = "discord";
-          workspace = "11 silent";
-          monitor = 1;
-        }
-        {
-          name = "Spotify";
-          "match:initial_class" = "spotify";
-          workspace = "12 silent";
-          monitor = 1;
-        }
-        {
-          name = "ClickUp";
-          "match:initial_class" = "ClickUp";
-          workspace = "13 silent";
-          monitor = 1;
-        }
-        {
-          name = "Obsidian";
-          "match:initial_class" = "obsidian";
-          workspace = "14";
-          monitor = 1;
-        }
-        {
-          name = "All";
-          "match:initial_class" = ".*";
-          suppress_event = "maximize";
-        }
-      ];
+      windowrule = import ./windowrules.nix;
     };
   };
 }
