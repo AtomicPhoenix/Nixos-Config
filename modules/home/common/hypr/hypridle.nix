24f028bbf6f8563fbc7a8e538b3202d5d48f3915
Possiblyai <imrana50280@gmail.com>
2026-02-19 15:15:01 -0500

    Cleaned and reorganized hypridle

--- modules/home/common/hypr/hypridle.nix
+++ modules/home/common/hypr/hypridle.nix
@@ -4,14 +4,9 @@ _: {
     settings = {
       # General settings for what to do when certain global events / signals occur (e.g. lock_cmd runs when a lock signal is heard)
       general = {
-        lock_cmd = "pidof hyprlock || hyprlock &;"; # avoid starting multiple hyprlock instances.
+        lock_cmd = "pidof hyprlock || hyprlock;"; # Avoid starting multiple hyprlock instances.
         before_sleep_cmd = "loginctl lock-session;";
-        after_sleep_cmd = ''
-          hyprctl dispatch dpms on;
-          nmcli radio wifi on;
-          brightnessctl -d intel_backlight s 100%;
-          brightnessctl -d card1-eDP-2-backlight s 100%;
-        '';
+        after_sleep_cmd = "hyprctl dispatch dpms on;";
         ignore_dbus_inhibit = false; # Don't ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
         ignore_systemd_inhibit = false; # Don't ignore systemd-inhibit --what=idle inhibitors
         ignore_wayland_inhibit = false; # Don't ignore wayland protocol inhibition requests (e.g. from running video players)
@@ -19,21 +14,20 @@ _: {
       listener = [
         {
           timeout = 120;
-          on-timeout = "brightnessctl -d intel_backlight s 30% &&  brightnessctl -d card1-eDP-2-backlight s 30%";
-          on-resume = "brightnessctl -d intel_backlight s 100% &&  brightnessctl -d card1-eDP-2-backlight s 100%";
+          on-timeout = "loginctl lock-session;";
         }
         {
-          timeout = 240;
-          on-timeout = "loginctl lock-session;";
-          on-resume = "hyprctl dispatch dpms on";
+          timeout = 180;
+          on-timeout = "(systemd-ac-power && set-brightness 50%) || hyprctl dispatch dpms off"; # Turns DPMS off when on battery
+          on-resume = "hyprctl dispatch dpms on; set-brightness 100%";
         }
         {
           timeout = 300;
-          on-timeout = "systemd-ac-power || hyprctl dispatch dpms off"; # Turns DPMS off when on battery
+          on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
         {
-          timeout = 600;
+          timeout = 360;
           on-timeout = "loginctl suspend";
         }
       ];
