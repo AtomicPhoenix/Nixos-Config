c6bba4388557a1e185fba23db3934f7ea40fad7f
Possiblyai <imrana50280@gmail.com>
2026-02-23 03:58:18 -0500

    Added fallback fonts and removed unused fonts

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -1,11 +1,17 @@
 {pkgs, ...}: {
   fonts = {
     packages = with pkgs; [
-      font-awesome
-      nerd-fonts.departure-mono
-      nerd-fonts.symbols-only
+      # Terminal/Programming font
       nerd-fonts.fira-code
-      nerd-fonts.caskaydia-cove
+
+      # Fallback font with large unicode coverage
+      noto-fonts
+      noto-fonts-cjk-sans # Chinese, Japanese, Korean sans rendering
+      noto-fonts-cjk-serif # Chinese, Japanese, Korean serif rendering
+      noto-fonts-color-emoji # Emoji Support
+
+      font-awesome # Icons
+      liberation_ttf # Open source equivalent of Microsoft fonts for Office compatability
     ];
   };
 }
