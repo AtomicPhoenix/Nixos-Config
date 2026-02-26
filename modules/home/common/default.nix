a40e208a6435887a4b5aae1aedaf5252075770a4
Possiblyai <imrana50280@gmail.com>
2026-02-26 05:09:36 -0500

    Removed wayland.nix by condensing into default.nix and removing redundant config options

--- modules/home/common/default.nix
+++ modules/home/common/default.nix
@@ -11,7 +11,6 @@
     ./rofi.nix
     ./tmux.nix
     ./waybar/waybar.nix
-    ./wayland.nix
   ];
 
   /**
@@ -41,5 +40,16 @@
     username = "ai";
     homeDirectory = lib.mkForce "/home/ai";
     stateVersion = "25.05";
+    sessionVariables = {
+      GDK_BACKEND = "wayland,x11";
+      QT_QPA_PLATFORM = "wayland;xcb";
+
+      #SDL_VIDEODRIVER = "x11";
+      CLUTTER_BACKEND = "wayland";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      WLR_NO_HARDWARE_CURSORS = "1";
+    };
   };
 }
