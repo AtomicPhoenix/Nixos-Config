658125e1e155bb179b401993bc9d1026f8ddce60
Possiblyai <imrana50280@gmail.com>
2026-05-12 22:25:21 -0400

    Fixed load_tmux command

--- modules/nixos/scripts/load_tmux.sh
+++ modules/nixos/scripts/load_tmux.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-# Grab an existing unattached session, if there are any
-unattached_session="$(tmux ls | grep -v attached | head -1 | cut -d: -f1)"
-# If no existing unattached sessions, create a new one
-if [[ -z $unattached_session ]]; then
+# Create new tmux server if one doesn't already exist
+if ! tmux has-session 2>/dev/null; then
+	tmux new-session
+	exit 0
+fi
+
+# Grab the ID of an existing unattached session (if one exists)
+unattached_session="$(tmux list-sessions -F "#{session_name}:#{session_attached}" 2>/dev/null | awk -F: '$2 == 0 {print $1; exit}')"
+
+if [[ -z "$unattached_session" ]]; then
+	# No unattached sessions; Create new session
 	tmux new-session
 else
-	# Attach to existing unattached session
+	# Attach to existing unattached sessio
 	tmux attach-session -t "$unattached_session"
 fi
